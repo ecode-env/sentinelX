3d1946d6f616e07ee791e19be9f04dfba84cf77a
eyobScript <eyobbmulugeta@gmail.com>
2025-08-20 01:36:15 +0300

    create nmap scanner and parse it to json

--- sentinelx_backend/config/config.py
+++ sentinelx_backend/config/config.py
@@ -7,3 +7,7 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
     ENABLE_NMAP = int(os.getenv('ENABLE_NMAP', 0))
     ENABLE_CLAMAV = int(os.getenv('ENABLE_CLAMAV', 0))
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", "sqlite:///sentinelx.db"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
